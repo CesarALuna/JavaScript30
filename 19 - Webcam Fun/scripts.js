b038ffb8aeb92c04a2f1509fb326ad62a53a1ffe
Cesar Luna <CesLun@gmail.com>
2022-10-06 00:04:16 -0700

    complete exercise 19

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -8,8 +8,65 @@ function getVideo() {
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: false })
     .then((localMediaStream) => {
-      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error(`Oh no!!`, err);
     });
 }
 
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+    // take pixels out
+    let pixels = ctx.getImageData(0, 0, width, height);
+    // mess with colors
+    // pixels = redEffect(pixels);
+    pixels = rgbSplit(pixels);
+    ctx.globalAlpha = 0.7;
+    // put them back
+    ctx.putImageData(pixels, 0, 0);
+  }, 16);
+}
+
+function takePhoto() {
+  // play the sound
+  snap.currentTime = 0;
+  snap.play();
+
+  // capture image data
+  const data = canvas.toDataURL("image/jpeg");
+  const link = document.createElement("a");
+  link.href = data;
+  link.setAttribute("download", "handsome");
+  link.innerHTML = `<img src='${data} alt='Hithere' />`;
+  strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 200; //r
+    pixels.data[i + 1] = pixels.data[i + 1] - 50; //g
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5; //b
+  }
+  return pixels;
+}
+
+function rgbSplit(pixels) {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 90] = pixels.data[i + 0]; //r
+    pixels.data[i + 100] = pixels.data[i + 1]; //g
+    pixels.data[i + 90] = pixels.data[i + 2]; //b
+  }
+  return pixels;
+}
+
 getVideo();
+
+video.addEventListener("canplay", paintToCanvas);
